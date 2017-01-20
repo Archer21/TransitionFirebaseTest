2d375c24f9914adc42b6b7c90705fdec2c1f7f60
Archer <yatogami18@email.com>
2017-01-20 11:48:33 -0400

    User now can take a photo or select an image from gallery, still need to resize the image. Next step: Upload image to Firebase Storage.

--- app/src/main/java/com/archer/transitionfirebasetest/view/activity/CreateNewPost.java
+++ app/src/main/java/com/archer/transitionfirebasetest/view/activity/CreateNewPost.java
@@ -1,13 +1,19 @@
 package com.archer.transitionfirebasetest.view.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.archer.transitionfirebasetest.MyApplication;
@@ -21,21 +27,35 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
 
 public class CreateNewPost extends BaseActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 100;
+    private static final int IMG_RESULT = 200;
     @BindView(R.id.content)
     EditText content;
 
+    @BindView(R.id.selected_image)
+    ImageView imageView;
+
     @BindView(R.id.spinner)
     MaterialSpinner spinner;
+
     @BindView(R.id.button)
     Button button;
+
+    private String mCurrentPhotoPath;
+    private String mCurrentAbsolutePhotoPath;
 
     private MyApplication app;
     private List<String> userCommunities;
@@ -62,8 +82,25 @@ public class CreateNewPost extends BaseActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Picasso.with(CreateNewPost.this).load(mCurrentPhotoPath).into(imageView);
+                galleryAddPic();
+            }
 
+            if (requestCode == IMG_RESULT) {
+
+                Uri selectedImageURI = data.getData();
+
+                Picasso.with(CreateNewPost.this).load(selectedImageURI).into(imageView);
+            }
+        }
+
+    }
 
     public void createNewPost (View view) {
         String body = content.getText().toString();
@@ -127,6 +164,73 @@ public class CreateNewPost extends BaseActivity {
 
         return communities;
     }
+
+    /**
+     * Methods for take a photo
+     */
+
+    private File createImageFile () throws IOException {
+        String timeStamps    = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamps + "_";
+        File   storageDir    = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        mCurrentAbsolutePhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
+
+    /**
+     * OnClick methods
+     */
+
+    public void takePhoto (View view) {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(
+                        CreateNewPost.this,
+                        "com.archer.transitionfirebasetest",
+                        photoFile);
+
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    public void selectPhotoFromGallery (View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(intent, IMG_RESULT);
+    }
+
+
+
 
     @Override
     public int getLayout() {
